35d4d3a898ed96b5152e0ae1482f71b5764319e1
Shilton <shilton.sistemas@gmail.com>
2020-05-03 11:15:12 -0300

    Styling repository details

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logoImg from '../../assets/img/logo.svg';
@@ -76,7 +77,7 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.id} href="teste">
+          <Link key={repository.id} to={`/repository/${repository.full_name}`}>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -88,7 +89,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={25} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/img/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParamsDTO {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParamsDTO>();
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={15} />
+          voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/8092749?s=460&u=c9a97ce63ab9829ee489cbe7bdb6d48129bb8081&v=4"
+            alt=""
+          />
+          <div>
+            <strong>{params.repository}</strong>
+            <p>description</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1000</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>50</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>60</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>item 1</strong>
+            <p>item 2</p>
+          </div>
+
+          <FiChevronRight size={25} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
