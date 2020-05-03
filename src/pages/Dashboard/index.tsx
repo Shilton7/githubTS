c7d425c2843bdac0ced9764e723b0736c768b605
Shilton <shilton.sistemas@gmail.com>
2020-05-02 21:50:47 -0300

    Listing repositories using api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,62 +1,65 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/img/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
+interface RepositoryDTO {
+  id: number;
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<RepositoryDTO[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<RepositoryDTO>(`repos/${newRepo}`);
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/8092749?s=460&u=c9a97ce63ab9829ee489cbe7bdb6d48129bb8081&v=4"
-            alt="logo_avatar"
-          />
+        {repositories.map((repository) => (
+          <a key={repository.id} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>rocketshoes</strong>
-            <p>Clone project of Netshoes using React and Redux (web)</p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={25} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/8092749?s=460&u=c9a97ce63ab9829ee489cbe7bdb6d48129bb8081&v=4"
-            alt="logo_avatar"
-          />
-
-          <div>
-            <strong>rocketshoes</strong>
-            <p>Clone project of Netshoes using React and Redux (web)</p>
-          </div>
-
-          <FiChevronRight size={25} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/8092749?s=460&u=c9a97ce63ab9829ee489cbe7bdb6d48129bb8081&v=4"
-            alt="logo_avatar"
-          />
-
-          <div>
-            <strong>rocketshoes</strong>
-            <p>Clone project of Netshoes using React and Redux (web)</p>
-          </div>
-
-          <FiChevronRight size={25} />
-        </a>
+            <FiChevronRight size={25} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
